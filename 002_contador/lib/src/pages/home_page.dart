b95473c7678735a44dfdcf732f50f84638a7a0e2
Carlos León <cleondorta@gmail.com>
2020-05-05 16:13:54 +0100

    Nuevo proyecto

---
+++ 002_contador/lib/src/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = TextStyle(fontSize: 25);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text('Número de clicks:', style: estiloTexto,),
+            Text('0', style: estiloTexto,),
+          ]  
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () { 
+          print('Hola mundo');
+        },
+      ),
+    );
+  }
+}
